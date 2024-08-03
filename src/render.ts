46fd87ae018646bf39db467e314fa71bb74f7c64
takejohn <takejohn@takejohn.jp>
2024-08-03 18:55:13 +0900

    Set background

--- src/render.ts
+++ src/render.ts
@@ -1,8 +1,12 @@
 import { Tile, TileSet } from './types.ts';
 import { decode, Image } from 'x/imagescript';
 
-const TILE_WIDTH = 15;
-const TILE_HEIGHT = 21;
+const TILE_BACKGROUND_IMAGE: Image = await decode(
+    await Deno.readFile('assets/mahjong-tile-background_15x21.png'),
+) as Image;
+
+const TILE_BACKGROUND_WIDTH = TILE_BACKGROUND_IMAGE.width;
+const TILE_BACKGROUND_HEIGHT = TILE_BACKGROUND_IMAGE.height;
 
 const TILE_MAP: Tile[][] = [
     ['1m', '2m', '3m', '4m', '5m', '6m', '7m', '8m', '9m', '0m'] as const,
@@ -15,16 +19,21 @@ const TILES_SPRITE: Image = await decode(
     await Deno.readFile('assets/mahjong-tiles_15x21.png'),
 ) as Image;
 
+const TILE_PICTURE_WIDTH = 15;
+const TILE_PICTURE_HEIGHT = 21;
+const TILE_PICTURE_X_OFFSET = 1;
+const TILE_PICTURE_Y_OFFSET = 3;
+
 const TILE_IMAGES = new Map<Tile, Image>(
     TILE_MAP.map((row, i) =>
         row.map((tile, j) =>
             [
                 tile,
                 TILES_SPRITE.clone().crop(
-                    TILE_WIDTH * j,
-                    TILE_HEIGHT * i,
-                    TILE_WIDTH,
-                    TILE_HEIGHT,
+                    TILE_PICTURE_WIDTH * j,
+                    TILE_PICTURE_HEIGHT * i,
+                    TILE_PICTURE_WIDTH,
+                    TILE_PICTURE_HEIGHT,
                 ),
             ] as const
         )
@@ -33,9 +42,17 @@ const TILE_IMAGES = new Map<Tile, Image>(
 
 export function renderTiles(tiles: TileSet) {
     const { hand } = tiles;
-    const image = new Image(TILE_WIDTH * hand.length, TILE_HEIGHT);
+    const image = new Image(
+        TILE_BACKGROUND_WIDTH * hand.length,
+        TILE_BACKGROUND_HEIGHT,
+    );
     for (const [i, tile] of hand.entries()) {
-        image.composite(TILE_IMAGES.get(tile)!, TILE_WIDTH * i);
+        image.composite(TILE_BACKGROUND_IMAGE, TILE_BACKGROUND_WIDTH * i, 0);
+        image.composite(
+            TILE_IMAGES.get(tile)!,
+            TILE_BACKGROUND_WIDTH * i + TILE_PICTURE_X_OFFSET,
+            TILE_PICTURE_Y_OFFSET,
+        );
     }
     return image;
 }
