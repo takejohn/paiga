67464e0d8f5b41110f5b77e5c933dc470cd72609
takejohn <takejohn@takejohn.jp>
2024-08-31 14:16:54 +0900

    Ignore .png suffix

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,18 @@ import { parseTiles } from './parse.ts';
 import { renderTiles } from './render.ts';
 const app = new Hono();
 
+function removeSuffix(s: string, suffix: string): string {
+    if (s.endsWith(suffix)) {
+        return s.slice(0, s.length - suffix.length);
+    } else {
+        return s;
+    }
+}
+
 app.get('/', (c) => c.text('Hello, world!\n'));
 
 app.get('/:pattern', async (c) => {
-    const pattern = c.req.param('pattern');
+    const pattern = removeSuffix(c.req.param('pattern'), '.png');
     const tiles = parseTiles(pattern);
     const image = renderTiles(tiles);
     const png = await image.encode();
